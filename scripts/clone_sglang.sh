0a06d356ad8f4c84b5db796f46c761972c9384be
RamezesDong <donghouze666@outlook.com>
2025-05-21 16:05:02 +0800

    fix: base ci

--- scripts/clone_sglang.sh
+++ scripts/clone_sglang.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 remote_repo=$1
-git_branch=$1
+git_branch=$2
 
 # if there is not sglang repo, clone it
 if [ ! -d "sglang" ]; then
-    git clone $remote_repo
+    git clone -b $git_branch $remote_repo sglang
 fi
 
-# checkout the branch
+# 进入目录后再checkout
+cd sglang
 git checkout $git_branch
