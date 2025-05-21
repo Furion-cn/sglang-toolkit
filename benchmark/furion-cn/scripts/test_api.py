76dbbc73dd5cb190beaa62f1e6a1619c7b109829
RamezesDong <donghouze666@outlook.com>
2025-05-21 14:57:52 +0800

    image ci

--- benchmark/furion-cn/scripts/test_api.py
+++ benchmark/furion-cn/scripts/test_api.py
@@ -1,6 +1,6 @@
 import openai
 client = openai.Client(
-    base_url="http://127.0.0.1:30000/v1", api_key="EMPTY")
+    base_url="http://127.0.0.1:8000/v1", api_key="EMPTY")
 
 # Chat completion
 response = client.chat.completions.create(
